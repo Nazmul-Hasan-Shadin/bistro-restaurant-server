6a3c09a285c52b0a5b70ca9ff0ca89830f07acf3
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-11-16 16:36:03 +0600

    admin role and authentication related api added

--- index.js
+++ index.js
@@ -1,11 +1,186 @@
 const express= require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app= express()
 const cors = require('cors')
+ const jwt = require('jsonwebtoken');
+
 require('dotenv').config()
 app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.wjj4omp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+ 
+    const userCollection= client.db('bistroDb').collection('users')
+  const menueCollection= client.db('bistroDb').collection('menue')
+  const reviewCollection= client.db('bistroDb').collection('reviews')
+  const cartCollection= client.db('bistroDb').collection('carts')
+  
+  
+  // middlewares
+
+const verifyToken= (req,res,next)=>{
+  if (!req.headers.authorization) {
+    return res.status(401).send({message: 'forbidden access'})
+  };
+
+
+  const token = req.headers.authorization.split(' ')[1]
+  if (!token) {
+    return res.status(401).send({message: 'forbidden access'})
+  }
+
+ jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+   
+   if (err) {
+    return res.status(401).send({message: 'forbidden access'})
+   }
+
+   req.decoded=decoded;
+   next()
+
+ })
+
+
+
+  
+
+}  
+
+  app.patch('/users/admin/:id',async(req,res)=>{
+    const id = req.params.id; 
+    const filter= {_id: new ObjectId(id)}
+    const updatedDoc= {
+      $set:{
+        role:'admin'
+      }
+    }
+
+  const result= await userCollection.updateOne(filter,updatedDoc)
+  res.send(result)
+
+  })
+
+
+  app.delete('/users/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query= {_id: new ObjectId(id)}
+   const result= await userCollection.deleteOne(query)
+   res.send(result)
+  })
+
+  //  user related api
+ 
+  app.get('/users',verifyToken, async(req,res)=>{
+
+
+    const result = await userCollection.find().toArray();
+    res.send(result)
+  })
+
+//  jwt related api
+ 
+ app.post('/jwt',async(req,res)=>{
+  const user= req.body
+  const token= jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+
+ res.send({token});
+
+ })
+
+
+
+// menu related api
+
+  app.get('/menu',async(req,res)=>{
+    const result=await menueCollection.find().toArray()
+    res.send(result)
+  })
+  app.get('/reviews',async(req,res)=>{
+    const result=await menueCollection.find().toArray()
+    res.send(result)
+  })
+// users related api
+
+app.post('/users',async(req,res)=>{
+  const user= req.body;
+  // inser email if user doesnot exists
+//  you can do this many ways (email unique,upsert ,checking)
+  const query= {email:user.email}
+  const existUser= await userCollection.findOne(query)
+  if (existUser) {
+    return res.send({message:'user already exists',insertedId:null})
+  }
+
+
+  const result= await userCollection.insertOne(user);
+  res.send(result)
+})
+
+
+
+  // carts collection
+
+app.get('/carts',async(req,res)=>{
+  const email= req.query.email;
+   const  query= {email:email}
+ 
+
+  const result=await cartCollection.find(query).toArray();
+  res.send(result)
+})
+
+  app.post('/carts',async(req,res)=>{
+    const cartItem= req.body;
+    const result= await cartCollection.insertOne(cartItem)
+       res.send(result)
+  })
+
+
+ app.delete('/carts/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query={_id: new ObjectId(id)}
+  const result= await cartCollection.deleteOne(query)
+   res.send(result)
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
