c3fb6cc696b1240c61ea70e1414b26f4bc5f0e15
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-11-20 18:38:54 +0600

    strip paayment api has added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express= require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app= express()
 const cors = require('cors')
  const jwt = require('jsonwebtoken');
 
-require('dotenv').config()
+ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+ console.log(stripe);
+
 app.use(cors());
 const port = process.env.PORT || 5000;
 
@@ -32,6 +35,7 @@ async function run() {
   const menueCollection= client.db('bistroDb').collection('menue')
   const reviewCollection= client.db('bistroDb').collection('reviews')
   const cartCollection= client.db('bistroDb').collection('carts')
+  const paymentCollection= client.db('bistroDb').collection('payments')
   
   
   // middlewares
@@ -239,6 +243,41 @@ app.get('/carts',async(req,res)=>{
    res.send(result)
  })
 
+//  PAYMENT INFORMATON
+
+ app.post('/create-payment-intent',async(req,res)=>{
+  const {price}=req.body;
+  const amount= parseInt(price *100)
+  console.log(amount);
+
+  const paymentIntent= await stripe.paymentIntents.create({
+    amount:amount,
+    currency:'usd',
+    payment_method_types: ['card']
+  })
+
+  res.send({
+    clientSecret:paymentIntent.client_secret
+  })
+
+  
+ })
+
+//  
+app.post('/payments',async(req,res)=>{
+   const payment = req.body;
+   console.log(payment);
+  const paymentResult= await paymentCollection.insertOne(payment)
+
+  // carefully delete each item from the cart
+  const query = {_id: {
+    $in:payment.cartIds.map(id=> new ObjectId(id))
+  } }
+
+  const deleteResult= await cartCollection.deleteMany(query)
+  res.send({paymentResult,deleteResult})
+
+})
 
 
 
